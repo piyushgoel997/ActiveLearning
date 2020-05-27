58848e24d2c5b626b61c03b77fcea9280f9073e7
piyush <piyushgoel997@gmail.com>
2020-05-27 18:38:34 -0400

    added code for query by committee

--- posterior_estimation.py
+++ posterior_estimation.py
@@ -1,3 +1,4 @@
+import math
 import random
 import time
 
@@ -10,7 +11,7 @@ def sigmoid(X, w):
     return 1 / (1 + np.exp(-np.dot(X, w)))
 
 
-def log_reg(X, Y, eta=1, max_steps=100):
+def log_reg(X, Y, eta=1, max_steps=200):
     # w = np.dot(np.dot(np.linalg.inv(np.dot(np.transpose(X), X)), np.transpose(X)), Y)
     w = np.random.rand(X.shape[1], )
     step = 0
@@ -112,17 +113,51 @@ def random_sampling(indices, probs):
 def uncertainty_sampling(indices, probs):
     ind = -1
     min_diff = 0.5
-    for i in range(len(probs)):
+    pr = probs[0]
+    for i in range(len(pr)):
         if i in indices:
             continue
-        diff = abs(0.5 - probs[i])
+        diff = abs(0.5 - pr[i])
         if diff < min_diff:
             min_diff = diff
             ind = i
     return ind
 
 
-def active_learning(X, Y, X_true, Y_true, true, sampling=random_sampling, iters=100):
+def query_by_committee(indices, probs):
+    # using vote entropy
+    # each row of probs is the probs of each committee member.
+    ind = -1
+    max_entropy = 0
+    C = probs.shape[0]
+    for i in range(probs.shape[1]):
+        if i in indices:
+            continue
+        vote0 = 0
+        vote1 = 0
+        for p in probs[:, i]:
+            if p > 0.5:
+                vote1 += 1
+            else:
+                vote0 += 1
+        entropy = 0
+        if vote0 != 0:
+            entropy -= (vote0 / C) * math.log(vote0 / C)
+        if vote1 != 0:
+            entropy -= (vote1 / C) * math.log(vote1 / C)
+
+        if entropy > max_entropy:
+            max_entropy = entropy
+            ind = i
+    return ind
+
+
+def majority_voting(probs):
+    return np.round(np.sum(np.round(probs), axis=0) / probs.shape[0])
+
+
+def active_learning(X, Y, X_true, Y_true, true, sampling=random_sampling, iters=100, metric=sse,
+                    num_committee=1):
     indices = set()
     for a in range(4):
         indices.add(a)
@@ -131,19 +166,24 @@ def active_learning(X, Y, X_true, Y_true, true, sampling=random_sampling, iters=
     comparison = []
     for j in range(iters):
         # print(len(indices))
-        w = log_reg(X[list(indices)], Y[list(indices)])
-        true_probs = sigmoid(X_true, w)
+        w = []
+        for _ in range(num_committee):
+            w.append(log_reg(X[list(indices)], Y[list(indices)]))
+        w = np.array(w)
+        # w = log_reg(X[list(indices)], Y[list(indices)])
+        # Majority voting is used to decide the outcome of the committee.
+        true_probs = majority_voting(sigmoid(X_true, w.T).T)
         acc.append(calc_accuracy(Y_true, true_probs))
         aucs.append(auc(roc(Y_true, true_probs)))
-        comparison.append(compare_posterirors(true, true_probs, max_metric))
+        comparison.append(compare_posterirors(true, true_probs, metric))
 
         prev_len = len(indices)
-        probs = sigmoid(X, w)
+        probs = sigmoid(X, w.T).T
         indices.add(sampling(indices, probs))
         if len(indices) == prev_len:
             break
-        final_result = sigmoid(X_true, w)
-    return acc, aucs, comparison, final_result
+        # final_result = sigmoid(X_true, w)
+    return acc, aucs, comparison
 
 
 def extend_array(arr, n):
@@ -151,9 +191,9 @@ def extend_array(arr, n):
     return arr[idx]
 
 
-def plot(rs, us, name, plt_name):
-    plt.plot(rs, label="Random Sampling")
-    plt.plot(us, label="Uncertainty Sampling")
+def plot(fn, name, plt_name, fn_names):
+    for f, n in zip(fn, fn_names):
+        plt.plot(f, label=n)
     plt.xlabel("Number of training examples")
     plt.ylabel(plt_name)
     plt.title(name + " - " + plt_name)
@@ -163,12 +203,9 @@ def plot(rs, us, name, plt_name):
 
 
 def run(d, mu0, covar0, mu1, covar1, name="", num_exp=100):
-    errors = []
-    errors1 = []
-    accuracies = []
-    accuracies1 = []
-    roc_aucs = []
-    roc_aucs1 = []
+    errors = [[], [], []]
+    accuracies = [[], [], []]
+    roc_aucs = [[], [], []]
     X_true, Y_true = gen_data(d, mu0, covar0, mu1, covar1, 10_000)
     pdf1 = multivariate_normal(mean=mu1, cov=covar1).pdf(X_true[:, 1:])
     pdf0 = multivariate_normal(mean=mu0, cov=covar0).pdf(X_true[:, 1:])
@@ -177,86 +214,49 @@ def run(d, mu0, covar0, mu1, covar1, name="", num_exp=100):
         print("Experiment number:", i + 1)
         exp_start_time = time.time()
         X, Y = gen_data(d, mu0, covar0, mu1, covar1, 200)
-        acc, aucs, comparison, final_result = active_learning(X, Y, X_true, Y_true, true, random_sampling)
-        acc1, aucs1, comparison1, final_result1 = active_learning(X, Y, X_true, Y_true, true, uncertainty_sampling)
-        print(len(comparison1))
-        if len(errors) == 0:
-            errors = np.array(comparison)
-            errors1 = extend_array(np.array(comparison1), len(errors))
-            accuracies = np.array(acc)
-            accuracies1 = extend_array(np.array(acc1), len(accuracies))
-            roc_aucs = np.array(aucs)
-            roc_aucs1 = extend_array(np.array(aucs1), len(roc_aucs))
+        acc, aucs, comparison = active_learning(X, Y, X_true, Y_true, true, random_sampling)
+        acc1, aucs1, comparison1 = active_learning(X, Y, X_true, Y_true, true, uncertainty_sampling)
+        print("Num of Queries made for Uncertainty Sampling:", len(comparison1))
+        acc2, aucs2, comparison2 = active_learning(X, Y, X_true, Y_true, true, query_by_committee, num_committee=11)
+        print("Num of Queries made for Query by Committee:", len(comparison2))
+        if len(errors[0]) == 0:
+            errors[0] = np.array(comparison)
+            errors[1] = extend_array(np.array(comparison1), len(errors[0]))
+            errors[2] = extend_array(np.array(comparison2), len(errors[0]))
+            accuracies[0] = np.array(acc)
+            accuracies[1] = extend_array(np.array(acc1), len(accuracies[0]))
+            accuracies[2] = extend_array(np.array(acc2), len(accuracies[0]))
+            roc_aucs[0] = np.array(aucs)
+            roc_aucs[1] = extend_array(np.array(aucs1), len(roc_aucs[0]))
+            roc_aucs[2] = extend_array(np.array(aucs2), len(roc_aucs[0]))
         else:
-            errors += np.array(comparison)
-            errors1 += extend_array(np.array(comparison1), len(errors))
-            accuracies += np.array(acc)
-            accuracies1 += extend_array(np.array(acc1), len(accuracies))
-            roc_aucs += np.array(aucs)
-            roc_aucs1 += extend_array(np.array(aucs1), len(roc_aucs))
+            errors[0] += np.array(comparison)
+            errors[1] += extend_array(np.array(comparison1), len(errors[0]))
+            errors[2] += extend_array(np.array(comparison2), len(errors[0]))
+            accuracies[0] += np.array(acc)
+            accuracies[1] += extend_array(np.array(acc1), len(accuracies[0]))
+            accuracies[2] += extend_array(np.array(acc2), len(accuracies[0]))
+            roc_aucs[0] += np.array(aucs)
+            roc_aucs[1] += extend_array(np.array(aucs1), len(roc_aucs[0]))
+            roc_aucs[2] += extend_array(np.array(aucs2), len(roc_aucs[0]))
         print("Time taken:", time.time() - exp_start_time)
-    errors /= num_exp
-    errors1 /= num_exp
-    accuracies /= num_exp
-    accuracies1 /= num_exp
-    roc_aucs /= num_exp
-    roc_aucs1 /= num_exp
 
-    # plotting the final posterior learned in the last experiment.
-    # plt.plot(X_true[:, 1:], final_result, '.', markersize=1, label="Random Sampling")
-    # plt.plot(X_true[:, 1:], final_result1, '.', markersize=1, label="Uncertainty Sampling")
-    # plt.plot(X_true[:, 1:], true, '.', markersize=1, label="True Posterior")
-    # plt.title(name + " - Posteriors")
-    # plt.legend()
-    # plt.savefig(name + " - Posteriors.jpg")
-    # plt.show()
+    errors = [e / num_exp for e in errors]
+    accuracies = [a / num_exp for a in accuracies]
+    roc_aucs = [r / num_exp for r in roc_aucs]
 
-    plot(errors, errors1, name, "Posterior Diff (SSE)")
-    plot(accuracies, accuracies1, name, "Accuracy")
-    plot(roc_aucs, roc_aucs1, name, "ROC AUC")
-
-    # plt.plot(acc, label="Random Sampling")
-    # plt.plot(acc1, label="Uncertainty Sampling")
-    # plt.xlabel("Number of training examples")
-    # plt.ylabel("Accuracy")
-    # plt.title(name + " - Accuracy")
-    # plt.legend()
-    # plt.savefig(name + " - Accuracy.jpg")
-    # plt.show()
-    #
-    # plt.plot(aucs, label="Random Sampling")
-    # plt.plot(aucs1, label="Uncertainty Sampling")
-    # plt.xlabel("Number of training examples")
-    # plt.ylabel("ROC AUC")
-    # plt.title(name + " - ROC AUC")
-    # plt.legend()
-    # plt.savefig(name + " - ROC AUC.jpg")
-    # plt.show()
-    #
-    # plt.plot(comparison, label="Random Sampling")
-    # plt.plot(comparison1, label="Uncertainty Sampling")
-    # plt.xlabel("Number of training examples")
-    # plt.ylabel("Posterior Diff - SSE")
-    # plt.title(name + " - Posterior Diff")
-    # plt.legend()
-    # plt.savefig(name + " - Posterior Diff.jpg")
-    # plt.show()
-    #
-    # plt.plot(X_true[:, 1:], final_result, '.', markersize=1, label="Random Sampling")
-    # plt.plot(X_true[:, 1:], final_result1, '.', markersize=1, label="Uncertainty Sampling")
-    # plt.plot(X_true[:, 1:], true, '.', markersize=1, label="True Posterior")
-    # plt.title(name + " - Posteriors")
-    # plt.legend()
-    # plt.savefig(name + " - Posteriors.jpg")
-    # plt.show()
+    fn_names = ["Random Sampling", "Uncertainty Sampling", "Query By Committee"]
+    plot(errors, name, "Posterior Diff (SSE)", fn_names)
+    plot(accuracies, name, "Accuracy", fn_names)
+    plot(roc_aucs, name, "ROC AUC", fn_names)
 
 
 start_time = time.time()
 d = 1
 mu0 = np.array([1])
 mu1 = np.array([2])
-covar0 = np.array([1.5]) * np.identity(d)
-covar1 = np.array([1]) * np.identity(d)
+covar0 = np.array([2]) * np.identity(d)
+covar1 = np.array([2]) * np.identity(d)
 run(d, mu0, covar0, mu1, covar1, name="Less Separation", num_exp=100)
 print("Total time taken:", time.time() - start_time)
 
@@ -269,7 +269,7 @@ print("Total time taken:", time.time() - start_time)
 
 # d = 4
 # mu0 = np.array([1, 2, 3, 4])
-# mu1 = np.array([2.5, 3.5, 4.5, 5.5])
+# mu1 = np.array([1.1, 1.8, 3.3, 3.6])
 # covar0 = np.array([1.5, 1.2, .5, 1]) * np.identity(d)
 # covar1 = np.array([1, 1.8, 0.9, 1.9]) * np.identity(d)
-# run(*gen_data(d, mu0, covar0, mu1, covar1), name="4D")
+# run(d, mu0, covar0, mu1, covar1, name="4D", num_exp=2)
